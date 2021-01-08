afd1e50561168bb171497848108c6eefdbb033be
josh110100100 <joshuahoffmann28@gmail.com>
2021-01-08 23:39:02 +0100

    Added Basic Datatypes

--- datatypes/main.go
+++ datatypes/main.go
@@ -3,5 +3,17 @@ package main
 import "fmt"
 
 func main() {
+	// Basic Types (Numbers, String, Variables)
 	fmt.Println("You are ready to go!")
+	// inferred variables
+	a := 1      //Int64
+	b := 1.0    //Float64
+	c := "Hi"   //String
+	d := 2i + 3 //complex128
+	fmt.Println(a,b,c,d)
+
+	// declared variables
+	var e string = "This is a string"
+
+	// Aggregate Types
 }
